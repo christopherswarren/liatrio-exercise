5298afad800f6f896165ef6273789e1a92beda08
Chris Warren <whoha42@gmail.com>
2021-12-21 12:26:56 -0600

    initial commit, api working

---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let moment = require('moment');
+let app = express();
+let port = process.env.port || 8080;
+
+app.get("/",function(request,response)
+{
+    response.json(
+        {
+            "message": "No root endpoint defined. Why not GET the /time ?"
+        }
+    )
+})
+
+app.get("/time",function(request,response)
+{
+    response.json(
+        {
+            "message": "Automate all the things!",
+            "timestamp": moment().unix()
+        }
+    );
+});
+
+app.listen(port, function () {
+    var datetime = new Date();
+    var message = "Server running on Port:- " + port + "Started at :- " + datetime;
+    console.log(message);
+});
